f27d12641492aeec990b3ae959fd17b00562e1ed
Renan Brian Hermenegildo Oliveira <renan.oliveira@patrimar.com.br>
2024-09-07 19:06:41 -0300

    criando chamados no zendesk e anexando arquivos

--- Entities/zendesk.py
+++ Entities/zendesk.py
@@ -3,6 +3,7 @@ import base64
 import os
 import pdb
 import json
+from typing import Literal, Dict, List, LiteralString
 
 import requests
 from Entities.dependencies.credenciais import Credential
@@ -15,8 +16,19 @@ class API:
     @property
     def token(self) -> str:
         phrase:bytes = f"{self.__user}/token:{self.__password}".encode("utf-8")
+        #phrase:bytes = f"rpa@patrimar.com.br/token:{self.__password}".encode("utf-8")
         encode_phrase:str = base64.b64encode(phrase).decode()
         return encode_phrase
+    
+    @property
+    def marca(self) -> Dict[str, dict]:
+        return {
+            "administrativo": {
+                "group_id": 26071794815383,
+                "brand_id": 25317468115479,
+            }
+        }
+        
 
     def __init__(self) -> None:
         crd:dict = Credential('API_ZENDESK').load()
@@ -42,24 +54,75 @@ class API:
 
         return {"status_code": response.status_code, "reason": response.reason, 'response': _response}
         
-    def add(self) -> dict:
+    def attachment(self, file_path:str) -> str:
+        try:
+            if not os.path.exists(file_path):
+                print(f"arquivo '{file_path}' não foi encontrado")
+                return ""
+                raise FileNotFoundError(f"arquivo '{file_path}' não foi encontrado")
+
+            reqUrl = f"https://patrimar.zendesk.com/api/v2/uploads?filename={os.path.basename(file_path)}"
+
+            headersList = {
+            "Authorization": f"Basic {self.token}",
+            "Content-Type": "application/octet-stream" 
+            }
+
+            payload = open(file_path, 'rb').read()
+
+            response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+            try:
+                return response.json().get('upload').get('token')
+            except Exception as error:
+                print(type(error), error)
+                return ""
+        except Exception as error:
+            print(type(error), error)
+            return ""
+        
+    def add(self, *, 
+            marca:Literal["administrativo"],
+            titulo:str,
+            descri:str,
+            ticket_form_id:int|None=None,
+            custom_fields:List[Dict[Literal["id", "value"],int|str]]=[],
+            attachment_path:str=""
+            ) -> dict:
         url = os.path.join(self.url, f"api/v2/tickets")
 
         headersList = {
         "Authorization": f"Basic {self.token}",
         "Content-Type": "application/json"
         }
-
-        payload = json.dumps({
-        "ticket": {
-                "subject": "Teste",
-            "raw_subject": "Teste",
-            "description": "Teste"
-        }
-        })
-
-        response = requests.request("POST", url, data=payload,  headers=headersList)
         
+        data:Dict[str,dict] = {
+            "ticket":{}
+        }
+        
+        data["ticket"]["requester_id"] = 26079230247703 #id do RPA
+        data["ticket"]["submitter_id"] = 26079230247703 #id do RPA
+        
+        data["ticket"]["group_id"] = self.marca[marca]['group_id']
+        data["ticket"]["brand_id"] = self.marca[marca]['brand_id']
+        data["ticket"]["subject"] = titulo
+        data["ticket"]["raw_subject"] = titulo
+        data["ticket"]["description"] = descri
+        if ticket_form_id:
+            data["ticket"]["ticket_form_id"] = ticket_form_id
+        if custom_fields:
+            data["ticket"]["custom_fields"] = custom_fields
+        
+        if (attachment_token:=self.attachment(attachment_path)):
+            data["ticket"]["comment"] =  {
+                #"body": "\n\n testando anexo 0013 \n\n\n",
+                "uploads": attachment_token
+            }
+
+        payload = json.dumps(data)
+        
+        response = requests.request("POST", url, data=payload,  headers=headersList)
+                
         try:
             _response = response.json()
         except:
@@ -86,4 +149,5 @@ class API:
 
         return {'status_code': response.status_code, 'reason': response.reason, 'response': _response}
         
-    
+if __name__ == "__main__":
+    pass
